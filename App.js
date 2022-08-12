03ccee48951753d9d6ded0ffd1912628cf6d3c6c
julian <juliangomez@Julians-MacBook-Pro.local>
2022-08-12 16:26:56 -0500

    ball in the center

--- App.js
+++ App.js
@@ -1,14 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Accelerometer } from 'expo-sensors';
+
 
 export default function App() {
+
+  const [data, setData] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
+  const [subscription, setSubscription] = useState(null);
+
+  const _slow = () => {
+    Accelerometer.setUpdateInterval(1000);
+  };
+
+  const _fast = () => {
+    Accelerometer.setUpdateInterval(16);
+  };
+
+  const _subscribe = () => {
+    setSubscription(
+      Accelerometer.addListener(accelerometerData => {
+        //console.log(accelerometerData)
+        setData(accelerometerData);
+      })
+    );
+  };
+
+  const _unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  useEffect(() => {
+    _subscribe();
+    return () => _unsubscribe();
+  }, []);
+
+  const { x, y, z } = data;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Mika is my dog!</Text>
+      <View style={[styles.ball, {top:scale(data.y, -1, 1, 0, Dimensions.get("window").height), left:scale(data.x, 
+        -1, 1, 0, Dimensions.get("window").width)} ]}></View>
+      <View ></View>
       <StatusBar style="auto" />
     </View>
+
+    
   );
+}
+
+function scale( number, inMin, inMax, outMin, outMax) {
+  return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +65,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  ball: {
+    width: 50,
+    height: 50,
+    backgroundColor:'blue',
+    position:'absolute',
+    borderRadius:25,
+  },
+  line: {
+    width:10,
+    backgroundColor:'black',
+  }
+
 });
